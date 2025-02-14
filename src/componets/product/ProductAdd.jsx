45a25402eb206cdf883614173f25a65c1b400d91
Deepndra Singh <sdeependra1234@gmail.com>
2025-02-14 18:35:48 +0530

    Products can filter by categories and price

--- src/componets/product/ProductAdd.jsx
+++ src/componets/product/ProductAdd.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addProduct } from "../../store/reducers/productSlice";
+import ProductCard from "./ProductCard";
 
 const ProductAdd = () => {
   const dispatch = useDispatch();
+  const [item, setItem] = useState([])
   const user = useSelector((state) => state.auth.user);
 
   // Show form only for sellers
@@ -24,11 +26,13 @@ const ProductAdd = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(addProduct(product));
+    setItem([...item, product])
     setProduct({ title: "", price: "", image: "", description:"", oldPrice:"" });
   };
 
   return (
-    <div className="p-6 max-w-md mx-auto">
+    <div className="w-screen flex items-center justify-center flex-col min-h-screen pt-[8vw]">
+      <div className="w-screen p-6 max-w-md mx-auto">
       <h2 className="text-xl font-bold mb-4">Add New Product</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
         <input
@@ -80,6 +84,20 @@ const ProductAdd = () => {
           Add Product
         </button>
       </form>
+      </div>
+      <div className="w-screen px-[5vw] py-[5vh] grid grid-cols-4 gap-10">
+        {item.map((item)=>(
+          <ProductCard
+          key = {item.id}
+          product = {item}
+          title ={item.title}
+          description ={item.description}
+          images = {item.image}
+          price = {item.price}
+          oldPrice = {item.oldPrice}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/componets/product/ProductSection.jsx
+++ src/componets/product/ProductSection.jsx
@@ -4,7 +4,11 @@ import { useSelector } from 'react-redux';
 
 
 const ProductSection = () => {
+  const [selectedCategory, setSelectedCategory] = useState("all")
+  const [sortOrder, setSortOrder] = useState("")
   const products = useSelector((state) => state.products.products);
+  // console.log(products?.category);
+  
   
     const { query } = useSelector((state) => state.search);
     const filteredProducts = query.trim()
@@ -12,13 +16,38 @@ const ProductSection = () => {
             product.title.toLowerCase().includes(query.toLowerCase())
           )
         : products;
-
+    
+    let categoriesProduct = selectedCategory === "all" ? [...products] : products.filter((product)=> product.category === selectedCategory)
+    console.log(categoriesProduct);
+    
+  if (sortOrder === "lowToHigh") {
+    categoriesProduct.sort((a, b) => a.price - b.price);
+  } else if (sortOrder === "highToLow") {
+    categoriesProduct.sort((a, b) => b.price - a.price);
+  }
+     
   return (
-    <div className=' my-10 grid grid-cols-4 gap-10 px-20'>
-      {filteredProducts ? (
-        filteredProducts.length > 0 ? (
-          filteredProducts.map((item) => (
-            <ProductCard
+    <>
+      <div className='w-screen flex items-center justify-between bg-white py-[1vh] px-[6vw] mt-[5vh]  '>
+        <div className='flex gap-[5vw] font-normal text-red-700'>
+          <h2 className='tracking-[5px]' onClick={() => setSelectedCategory("all")} >ALL PRODUCTS</h2>
+          <h2 className='tracking-[5px]' onClick={() => setSelectedCategory("men")} >MEN</h2>
+          <h2 className='tracking-[5px]' onClick={() => setSelectedCategory("women")} >WOMEN</h2>
+          <h2 className='tracking-[5px]' onClick={() => setSelectedCategory("kids")} >KIDS</h2>
+          <h2 className='tracking-[5px]'>NEW ARRIVALS</h2>
+        </div>
+        <div>
+          <select onChange={(e) => setSortOrder(e.target.value)} value={sortOrder}>
+              <option value="">Sort By Price</option>
+              <option value="lowToHigh">Low to High</option>
+              <option value="highToLow">High to Low</option>
+          </select>
+        </div>
+      </div>
+      <div className=' my-10 grid grid-cols-4 gap-10 px-20'>
+        {categoriesProduct.length > 0 ? (
+          categoriesProduct.map((item)=> (
+            <ProductCard 
             key = {item._id}
             product = {item}
             title ={item.title}
@@ -26,25 +55,39 @@ const ProductSection = () => {
             images = {item.image}
             price = {item.price}
             oldPrice = {item.oldPrice}
-            />
+             />
           ))
-        ) : (
-          <p className="col-span-full text-center text-gray-500">No products found</p>
-        )
-      ) : (
-        products.map((item)=>(
-          <ProductCard 
-          key = {item._id}
-          product = {item}
-          title ={item.title}
-          description ={item.description}
-          images = {item.image}
-          price = {item.price}
-          oldPrice = {item.oldPrice}
-           />
-        ))
-      )}
+        ): (
+          filteredProducts ? (
+            filteredProducts.length > 0 ? (
+              filteredProducts.map((item) => (
+                <ProductCard
+                key = {item._id}
+                product = {item}
+                title ={item.title}
+                description ={item.description}
+                images = {item.image}
+                price = {item.price}
+                oldPrice = {item.oldPrice}
+                />
+              ))
+            ) : (
+              <p className="col-span-full text-center text-gray-500">No products found</p>
+            )
+          ) :  (products.map((item)=>(
+            <ProductCard 
+            key = {item._id}
+            product = {item}
+            title ={item.title}
+            description ={item.description}
+            images = {item.image}
+            price = {item.price}
+            oldPrice = {item.oldPrice}
+             />
+          )) )
+        )}
     </div>
+    </>
   )
 }
 
